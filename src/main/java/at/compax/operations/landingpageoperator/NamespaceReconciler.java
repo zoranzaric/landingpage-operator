f38318013fab5d56f641a80c114b92a92b57a908
Zoran Zaric <zoran.zaric@compax.at>
2022-05-19 01:14:39 +0200

    update

--- src/main/java/at/compax/operations/landingpageoperator/NamespaceReconciler.java
+++ src/main/java/at/compax/operations/landingpageoperator/NamespaceReconciler.java
@@ -1,19 +1,13 @@
 package at.compax.operations.landingpageoperator;
 
-import java.util.Collections;
-import java.util.List;
-
 import io.fabric8.kubernetes.api.model.Namespace;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
-import io.javaoperatorsdk.operator.api.reconciler.EventSourceContext;
-import io.javaoperatorsdk.operator.api.reconciler.EventSourceInitializer;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
-import io.javaoperatorsdk.operator.processing.event.source.EventSource;
 
 @ControllerConfiguration
-public class NamespaceReconciler implements Reconciler<Namespace>, EventSourceInitializer<Namespace> {
+public class NamespaceReconciler implements Reconciler<Namespace>, ResourceContro {
 
   public NamespaceReconciler() {
   }
@@ -23,16 +17,11 @@ public class NamespaceReconciler implements Reconciler<Namespace>, EventSourceIn
     System.out.println("reconcile namespace " + namespace.getMetadata().getName());
     String url = namespace.getMetadata().getLabels().get("landingpage-url");
     if (url != null) {
-      System.out.println("landingpage-url: " + url);
+      System.out.println(namespace.getMetadata().getName() + " landingpage-url=" + url);
     } else {
-      System.out.println("landingpage-url: [unset]");
+      System.out.println(namespace.getMetadata().getName() + " landingpage-url-");
     }
 
     return UpdateControl.noUpdate();
-  }
-
-  @Override
-  public List<EventSource> prepareEventSources(EventSourceContext<Namespace> eventSourceContext) {
-    return Collections.emptyList();
   }
 }
